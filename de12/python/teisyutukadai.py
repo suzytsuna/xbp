ec3dd672ab62e7c4152a103cd70d4cbc4db72307
suzytsuna <s202302213@jindai.jp>
2023-10-29 16:20:48 +0900

    20230129

--- de12/python/teisyutukadai.py
+++ de12/python/teisyutukadai.py
@@ -1,12 +1,22 @@
+print ("チームの勝敗がかかったＰＫ！さあ、どうする！？")
+name=input("プレーヤーの名前を入れてください")
+print(name,"さんはキーパーです")
+
+c=0
 for i in range (3):
     print(i,"回目")
    
-    b=int(input("0~5の中で好きな数字を選んでね"))
-    print(b)
-    import random
-    a =int( random.randint(0, 5))
+    a=int(input("1,右2,真ん中3,左の中で好きな数字を選んでね"))
     print(a)
-    if a==b:
-        print("You WIN!!")
-    else:
-        print("You LOST")
+    import random
+    b =int( random.randint(1, 3))
+    print(b)
+    if a==b:#コンピューターと同じ数字を選んだ
+        c=c+1
+        print("とめた！")
+    else:#違う数字を選んだ
+        print("逃した...")
+    if c>=2:#３回中２回以上勝った
+        print("おめでとう！あなたはチームを勝利へ導いた英雄として称えられた！！")
+    else:#３回中２回負けた
+        print("残念、もう一度このゲームにチャレンジしよう！")
