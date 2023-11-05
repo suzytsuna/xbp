d305c0b572da0f03121469ef8cba849db3beffea
suzytsuna <s202302213@jindai.jp>
2023-11-05 11:38:39 +0900

    20231105

--- de12/python/test2.py
+++ de12/python/test2.py
@@ -2,16 +2,18 @@ import librosa
 import numpy as np
 import soundfile as sf
 
+# WAVファイルのパス
+wav_file = 'xbp/de12/python/aaa.wav'  # 正しいファイル名を指定
+
 # 音声ファイルを読み込む
-audio_file = 'xbp/de12/python/aaa.wav'  # あなたがダウンロードした音声ファイルへのパス
-y, sr = librosa.load(audio_file)
+y, sr = librosa.load(wav_file)
 
 # 主旋律を抽出
-melody, _ = librosa.effects.pitches(y=y, sr=sr)
+melody, _ = librosa.piptrack(y=y, sr=sr)
 melody = np.nan_to_num(melody)  # NaNを0に置き換える
 
 # 主旋律を保存
-output_file = '主旋律.wav'
+output_file = 'C:/Users/suzyt/mygit/xbp/de12/python/aaa.wav'
 sf.write(output_file, melody, sr)
 
 print('主旋律を抽出し、保存しました。')
